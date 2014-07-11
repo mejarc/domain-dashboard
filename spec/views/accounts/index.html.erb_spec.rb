c602f0a95efc566ba8d220907767aac9417e373f
Melanie Archer <job@twobanjos.com>
2014-07-11 16:22:26 -0700

    Revising Account View Spec- #8
    * remove duplicate nodes (won't pass validation)
    * closes #8

--- spec/views/accounts/index.html.erb_spec.rb
+++ spec/views/accounts/index.html.erb_spec.rb
@@ -3,17 +3,12 @@ require 'rails_helper'
 RSpec.describe "accounts/index", :type => :view do
   before(:each) do
     assign(:accounts, [
-      Account.create!(
-        :name => "Name"
-      ),
-      Account.create!(
-        :name => "Name"
-      )
+      Account.create!(:name => "Test Name")
     ])
   end
 
   it "renders a list of accounts" do
     render
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
+    assert_select "tr>td", :text => "Test Name".to_s
   end
 end
